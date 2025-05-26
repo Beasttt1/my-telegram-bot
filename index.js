b396c1a90e22f461699f9c3a4a3f9ca71f49aad7
Beasttt1 <amir.mv.7690@gmail.com>
2025-05-27 00:45:18 +0330

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { getDatabase, ref, set, get, update, remove, push } = require('firebase/d
 
 const app = express();
 const { startChallenge, handleAnswer } = require('./challenge');
-const { fetchLatestNews } = require('./news');
+const { sendNews } = require('./news');
 const { handlePickCommand, handlePickRole, handlePickAccessConfirmation } = require('./pick');
 // فرض بر این است که bot, db, updatePoints, adminId قبلاً تعریف شده دکمه‌ها (callback_query):
 const token = process.env.BOT_TOKEN;
