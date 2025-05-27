494da5ed47ad1c7ee7526ad4c4f4479638ef5c03
Beasttt1 <amir.mv.7690@gmail.com>
2025-05-27 04:07:02 +0330

    Update index.js

--- index.js
+++ index.js
@@ -381,7 +381,8 @@ bot.on('callback_query', async (query) => {
   const data = query.data;
   const messageId = query.message && query.message.message_id;
   userState[userId] = null;
-  sendMainMenu(userId, msg.from); // اینجا msg.from را اضافه کن
+  sendMainMenu(userId, msg.from);
+});
   const blockedBtn = MENU_BUTTONS.find(btn => btn.key === data);
 if (blockedBtn && !(await isButtonEnabled(data)) && userId !== adminId) {
   return bot.answerCallbackQuery(query.id, { text: 'این بخش موقتا از دسترس خارج شده', show_alert: true });
